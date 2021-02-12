347434ff38ec960a3bd4e16b173e0676aba1f47d
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2021-02-12 12:41:45 +0100

    QC for PTMs is not only labile PTMs, changed the plot legends/titles

--- bin/qc_ptms.R
+++ bin/qc_ptms.R
@@ -6,7 +6,7 @@ library(stringr)
 
 args = commandArgs(trailingOnly=TRUE)
 nrsets = as.numeric(args[1])
-labileptms = unlist(strsplit(args[2], ';'))
+usedptms = unlist(strsplit(args[2], ';'))
 psmfn = args[3]
 pepfn = args[4]
 
@@ -20,7 +20,7 @@ set_amount_psms = aggregate(SpecID~Biological.set, psms, length)
 svg('ptmpsmfeats', width=width, height=nrsets+2)
   print(ggplot(set_amount_psms) +
     geom_bar(aes(Biological.set, y=SpecID), stat='identity') +
-    coord_flip() + ylab('# PSMs with labile PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
+    coord_flip() + ylab('# PSMs with PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
   )
 dev.off()
 
@@ -33,7 +33,7 @@ pepnums = aggregate(value~variable, set_amount_pep, length)
 svg('ptmpepfeats', width=width, height=nrsets+2)
   print(ggplot(pepnums) +
     geom_bar(aes(variable, y=value), stat='identity') +
-    coord_flip() + ylab('# peptides with labile PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
+    coord_flip() + ylab('# peptides with PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
   )
 dev.off()
 
@@ -46,7 +46,7 @@ if ('Master.protein.s.' %in% names(psms)) {
     svg('ptmprotfeats', width=width, height=nrsets+2)
       print(ggplot(set_amount_prots) +
         geom_bar(aes(Biological.set, y=Master.protein.s.), stat='identity') +
-        coord_flip() + ylab('# proteins with labile PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
+        coord_flip() + ylab('# proteins with PTM') + theme_bw() + theme(axis.title=element_text(size=15), axis.text=element_text(size=10), axis.title.y=element_blank(), legend.text=element_text(size=10), legend.title=element_blank(), legend.position='top', plot.title=element_text(size=15))
       )
     dev.off()
 }
@@ -61,7 +61,7 @@ sites = data.frame(site=unlist(multisites),
                    bioset=rep(sites[,1], sapply(multisites, FUN=length)),
                    ptm=rep(sites[,2], sapply(multisites, FUN=length))
                    )
-sites = subset(sites, ptm %in% labileptms)
+sites = subset(sites, ptm %in% usedptms)
 sites$site = sub('[0-9]+', '', sites$site)
 sites$value=1
 sites$site = paste(sites$ptm, sites$site)
@@ -83,7 +83,7 @@ sites = data.frame(site=unlist(multisites),
                    bioset=rep(sites[,1], sapply(multisites, FUN=length)),
                    ptm=rep(sites[,2], sapply(multisites, FUN=length))
                    )
-sites = subset(sites, ptm %in% labileptms)
+sites = subset(sites, ptm %in% usedptms)
 sites$site = sub('[0-9]+', '', sites$site)
 sites$value=1
 sites$site = paste(sites$ptm, sites$site)
