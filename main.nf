lehtiolab/ddamsproteomics

commit 9fdf60701701884224f2ba6c95c3653a5884148e

Sort output of merged sets so they end up alphabetic order

main.nf
@@ -1123,7 +1123,7 @@ process mergePTMPeps {
   peptable = 'ptm_peptidetable.txt'
   """
   cat ptmlup.sql > pepptmlup.sql
-  msstitch merge -i ${peptides.join(' ')} --setnames ${setnames.join(' ')} --dbfile pepptmlup.sql -o mergedtable --no-group-annotation \
+  msstitch merge -i ${peptides.join(' ')} --setnames ${setnames.sort().join(' ')} --dbfile pepptmlup.sql -o mergedtable --no-group-annotation \
     --fdrcolpattern '^q-value' --flrcolpattern 'FLR' \
     ${!params.noquant && !params.noms1quant ? "--ms1quantcolpattern area" : ''} \
     ${!params.noquant && setisobaric ? "--isobquantcolpattern plex" : ''}
@@ -1284,7 +1284,7 @@ process proteinPeptideSetMerge {
   """
   # SQLite lookup needs copying to not modify the input file which would mess up a rerun with -resume
   cat $lookup > db.sqlite
-  msstitch merge -i ${tables.join(' ')} --setnames ${setnames.join(' ')} --dbfile db.sqlite -o mergedtable \
+  msstitch merge -i ${tables.join(' ')} --setnames ${setnames.sort().join(' ')} --dbfile db.sqlite -o mergedtable \
     --fdrcolpattern '^q-value\$' ${acctype != 'peptides' ? '--mergecutoff 0.01' : ''} \
     ${!params.noquant && !params.noms1quant ? "--ms1quantcolpattern area" : ''} \
     ${!params.noquant && setisobaric ? "--isobquantcolpattern plex" : ''} \

commit d12de2d1ee0b4b772a5a3d483349b52253e48a94

Fix bug in merge which was introduced in 9fdf607 to sort setnames but not tables, so they get wrongly aligned

main.nf
@@ -1366,7 +1366,8 @@ process proteinPeptideSetMerge {
 
   # SQLite lookup needs copying to not modify the input file which would mess up a rerun with -resume
   cat $lookup > db.sqlite
-  msstitch merge -i ${tables.join(' ')} --setnames ${setnames.sort().join(' ')} --dbfile db.sqlite -o mergedtable \
+  msstitch merge -i ${[tables, setnames].transpose().sort({ a,b -> a[1] <=> b[1]}).collect() { it[0] }.join(' ')} \
+    --setnames ${setnames.sort().join(' ')} --dbfile db.sqlite -o mergedtable \
     --fdrcolpattern '^q-value\$' ${acctype != 'peptides' ? '--mergecutoff 0.01' : ''} \
     ${!params.noquant && !params.noms1quant ? "--ms1quantcolpattern area" : ''} \
     ${!params.noquant && setisobaric ? "--isobquantcolpattern plex" : ''} \