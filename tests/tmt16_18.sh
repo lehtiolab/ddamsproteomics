b69035149f21965e8773142871cf3cac4461de8e
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2024-04-08 10:58:35 +0200

    Fix test due to new mzml set names

--- tests/tmt16_18.sh
+++ tests/tmt16_18.sh
@@ -6,9 +6,9 @@ echo TMT 16/18 mix, multi DB run
 name=tmt16_18mix
 baseresults=test_output/${name}
 nextflow run -resume -profile test ${repodir}/main.nf --name ${name} --outdir ${baseresults} \
-    --mzmldef <(cat "${testdir}/tmt16_mzmls.txt" <(sed 's/0set-A/0setB/' "${testdir}/tmt18_mzmls.txt") | envsubst) \
+    --mzmldef <(cat "${testdir}/tmt16_mzmls.txt" <(sed 's/0set-A/20set-A/' "${testdir}/tmt18_mzmls.txt") | envsubst) \
     --sampletable "${testdir}/tmt18_setAB_samples.txt" \
-    --isobaric '0set-A:tmt16plex:126:131N 0setB:tmt18plex:131' \
+    --isobaric '0set-A:tmt16plex:126:131N 20set-A:tmt18plex:131' \
     --tdb "${testdata}/tmt16_fa.fa" \
     --mods 'carbamidomethyl;oxidation' \
     --psmconflvl 0.4 --pepconflvl 0.4
